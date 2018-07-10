a84f2c302486a67cba102a25e854cca909f21d5b
Terry Teng <terry.teng@oocl.com>
2018-07-10 20:13:54 +0800

    [Terry] filter finshed

--- src/main/java/com/thoughtworks/collection/Filter.java
+++ src/main/java/com/thoughtworks/collection/Filter.java
@@ -4,6 +4,7 @@ import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class Filter {
 
@@ -14,18 +15,22 @@ public class Filter {
     }
 
     public List<Integer> filterEven() {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return array.stream().filter(x->x%2==0).collect(Collectors.toList());
     }
 
     public List<Integer> filterMultipleOfThree() {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return array.stream().filter(x->x%3==0).collect(Collectors.toList());
     }
 
     public List<Integer> getCommonElements(List<Integer> firstList, List<Integer> secondList) {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return firstList.stream().filter(x->secondList.contains(x)).collect(Collectors.toList());
     }
 
     public List<Integer> getDifferentElements() {
-        throw new NotImplementedException();
+        //throw new NotImplementedException();
+        return array.stream().distinct().collect(Collectors.toList());
     }
 }
